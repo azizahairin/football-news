87cdb4644deba9a8c2e2ac36d99f90f7c38aec8f
azizahairin <azizahkhairinniswah747@gmail.com>
2025-11-12 16:36:18 +0700

    tutorial 8 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-wa+s4(xk%gi7zgl(s+utl+=f4lk$naez_wne^apj+t6rj!9h(2
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","azizah-khairinniswah-footballnews.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","azizah-khairinniswah-footballnews.pbp.cs.ui.ac.id", "10.0.2.2"]
 CSRF_TRUSTED_ORIGINS = [
     "https://azizah-khairinniswah-footballnews.pbp.cs.ui.ac.id"
 ]
@@ -46,9 +46,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -155,4 +158,9 @@ else:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#-----
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
--- football_news/urls.py
+++ football_news/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls import path, include
+from authentication.views import login, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include('main.urls')),
+    path('', include('main.urls')),
+    path('auth/', include('authentication.urls')),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
 ]
